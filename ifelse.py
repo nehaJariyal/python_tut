1684407a883d8293e339f2b036c2fdaa55e63500
nehaJariyal <130833845+nehaJariyal@users.noreply.github.com>
2026-06-14 18:18:11 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,30 @@
+age = 10
+
+if(age >=18):
+    print("You are an adult.")
+elif(age >=13):
+        print("You are a teenager.")
+else: 
+       print("You are a child.")
+      
+
+light = "red"
+
+if(light == "red"):
+    print("Stop")
+elif(light == "yellow"):
+    print("Get ready")
+elif(light == "green"):
+    print("Go")    
+else:    print("i don't know that color")
+
+# nested if else
+
+
+number = 5
+if(number > 0):
+    print("The number is positive.")
+    if(number % 2 == 0):
+        print("The number is even.")
+    else:
+        print("The number is odd.")
