5a83d42b45ae4f7d464adb7cd5ffaffe70cd3c7b
nehaJariyal <130833845+nehaJariyal@users.noreply.github.com>
2026-06-14 18:20:52 +0530

    first commit
    my first commit

---
+++ stringAndItsMethod.py
@@ -0,0 +1,17 @@
+ 
+name = "neha"
+age = 20
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.title())
+print(name.swapcase())
+print(name.count("e"))
+print(name.find("h"))   
+print(name.replace("n", "m"))
+print(name.startswith("n"))
+print(name.endswith("a"))
+print(name.isalpha())
+print(name.isdigit())
+print(name.isalnum())
+print(name.isspace())
