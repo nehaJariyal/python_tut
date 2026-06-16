e63f17fff65c146332ad7043621e599426b983a9
Neha-ATR1305 <neha.kumari@antiersolutions.com>
2026-06-16 11:25:16 +0530

    learn opps concept

--- class.py
+++ class.py
@@ -27,3 +27,77 @@ s1.hello()
 
 # hidden the implemetation  detail  of a class  and only  showing  the essential feature  to the user
 #  hide unnessary thing  like A  car  we know only cluch  break  acessltor we dont know   internal process 
+
+class Car:
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch=False
+        
+    def start(self):
+       self.clutch = True
+       self.acc = True
+       print("car started  ")
+       
+car1 =Car()
+car1.start()
+
+# encapuslation 
+# wrapping  data and function into a single unit object 
+
+
+class Account:
+    def __init__(self, bal, acc):
+        self.balance = bal
+        self.account_no = acc
+
+    def credit(self, amt):
+        self.balance += amt
+        print("Rs.", amt, "was credited")
+        print("Total balance =", self.get_bal())
+
+    def debit(self, amt):
+        self.balance -= amt
+        print("Rs.", amt, "was debited")
+        print("Total balance =", self.get_bal())
+
+    def get_bal(self):
+        return self.balance
+
+
+account1 = Account(1000000, 1001001)
+
+account1.credit(100000)
+account1.debit(100)
+
+
+
+# delete key word  use for delete  object properties or object itself 
+
+class Sutdents:
+    def __init__(self, name):
+        self.name = name
+        
+s1 = Sutdents("neha")
+print(s1.name)
+# del s1.name
+print(s1.name)
+        #  how to  make private data  (self.__bank_account_password )  we can assess 
+class Account_bank:
+    def __init__(self, acont_no , account_pass):
+        self.bank_account_no = acont_no
+        self.__bank_account_password =account_pass
+        
+detl1 =Account_bank(123456,"Admin@123")
+print(detl1.bank_account_no)
+print(detl1.__bank_account_password)
+
+class Person:
+    __name = "neha"
+    
+    def __hello():
+        print("hello")
+        
+p1 = Person()
+
+print(p1.__name)
